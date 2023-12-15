d0b964331007d9c425325c3afcb3d800307d9cf0
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-12-15 17:22:16 +0300

    add add_to_cart tests

--- tests/test_add_item_to_cart_api.py
+++ tests/test_add_item_to_cart_api.py
@@ -13,25 +13,75 @@ def demowebshop_POST_request(url, **kwargs):
     return response
 
 
-def open_cart(self):
-    browser.open('/cart')
-
-
-def test_login(set_browser):
-    with step("Login with API"):
+def test_login():
+    with step('Login with API'):
         response = demowebshop_POST_request('/login',
             data={"Email": LOGIN, "Password": PASSWORD, "RememberMe": False},
             allow_redirects=False
         )
 
-    with step("Get cookie from API"):
-        cookie = response.cookies.get("NOPCOMMERCE.AUTH")
+    with step('Get cookie from API'):
+        cookie = response.cookies.get('NOPCOMMERCE.AUTH')
 
-    with step("Set cookie from API"):
+    with step('Set cookie from API'):
         browser.open('/')
         browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": cookie})
         browser.open('/')
 
-    with step("Verify successful authorization"):
-        browser.element(".account").should(have.text(LOGIN))
+    with step('Verify successful authorization'):
+        browser.element('.account').should(have.text(LOGIN))
+
+def test_add_one_item_to_cart():
+    with step('Add item through API'):
+        response = demowebshop_POST_request('/addproducttocart/catalog/45/1/1')
+
+    with step('Get cookie from API'):
+        cookie = response.cookies.get('Nop.customer')
+
+    with step('Set cookie from API'):
+        browser.open('/')
+        browser.driver.add_cookie({"name": "Nop.customer", "value": cookie})
+        browser.open('/cart')
+
+    with step('Check added item in cart'):
+        browser.all('.cart-item-row').should(have.size(1))
+        browser.element('.product-name').should(have.exact_text('Fiction'))
+        browser.element('.qty-input').should(have.value('1'))
+
+
+def test_add_two_different_items_to_cart():
+    with step('Add two different items through API'):
+        response = demowebshop_POST_request('/addproducttocart/catalog/45/1/1')
+        cookie = response.cookies.get('Nop.customer')
+        response = demowebshop_POST_request('addproducttocart/catalog/14/1/1', cookies={"Nop.customer": cookie})
+
+    with step('Get cookie from API'):
+        cookie = response.cookies.get('Nop.customer')
+
+    with step('Set cookie from API'):
+        browser.open('/')
+        browser.driver.add_cookie({"name": "Nop.customer", "value": cookie})
+        browser.open('/cart')
+
+    with step('Check added item in cart'):
+        browser.all('.cart-item-row').should(have.size(2))
+
+def test_add_three_same_item_to_cart():
+    with step('Add item through API'):
+        response = demowebshop_POST_request('addproducttocart/details/43/1', data = {"addtocart_43.EnteredQuantity": 3})
+
+    with step('Get cookie from API'):
+        cookie = response.cookies.get('Nop.customer')
+
+    with step('Set cookie from API'):
+        browser.open('/')
+        browser.driver.add_cookie({"name": "Nop.customer", "value": cookie})
+        browser.open('/cart')
+
+    with step('Check added item in cart'):
+        browser.all('.cart-item-row').should(have.size(1))
+        browser.element('.product-name').should(have.exact_text('Smartphone'))
+        browser.element('.qty-input').should(have.value('2'))
+
+
 
